7e3440286e993ada8a99271486c01365f9b7bfcc
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-02-17 21:51:34 +0700

    define some const

---
+++ PingPongGame/pre.h
@@ -0,0 +1,22 @@
+#ifndef PRE_H_
+#define PRE_H_
+
+#include "iostream"
+using namespace std;
+
+#include "SFML/Graphics.hpp"
+using namespace sf;
+
+#include "string"
+#include "vector"
+
+#define HEIGHT_SCREEN 1000
+#define WIDTH_SCREEN 1500
+
+#define SPEED_BALL 0.5f
+#define SIZE_BALL Vector2f(20, 20)
+
+#define SIZE_PADDLE Vector2f(30, 150)
+#define SPEED_PADDLE 1.0f
+
+#endif
