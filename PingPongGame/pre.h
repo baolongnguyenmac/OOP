fc1b7bbff96fe4e6baab4f0d7428ce80b05bdfe0
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-02-18 10:16:06 +0700

    define pos_ball

--- PingPongGame/pre.h
+++ PingPongGame/pre.h
@@ -13,6 +13,7 @@ using namespace sf;
 #define HEIGHT_SCREEN 1000
 #define WIDTH_SCREEN 1500
 
+#define POS_BALL Vector2f(WIDTH_SCREEN / 2, HEIGHT_SCREEN / 2)
 #define SPEED_BALL 0.5f
 #define SIZE_BALL Vector2f(20, 20)
 #define LEFT_UP 1
@@ -22,7 +23,9 @@ using namespace sf;
 #define LEFT_DOWN 5
 #define LEFT 6
 
-#define SIZE_PADDLE Vector2f(30, 150)
+#define SIZE_PADDLE Vector2f(25, 200)
 #define SPEED_PADDLE 1.0f
+#define POS_PADDLE_1 Vector2f(0, HEIGHT_SCREEN / 2 - SIZE_PADDLE.y / 2)
+#define POS_PADDLE_2 Vector2f(WIDTH_SCREEN - SIZE_PADDLE.x, HEIGHT_SCREEN / 2 - SIZE_PADDLE.y / 2)
 
 #endif
