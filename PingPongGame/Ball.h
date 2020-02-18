05d59a8f977377f8eab7dbe6b9104a6d8d91a0fd
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-02-18 10:14:48 +0700

    logic of ball

--- PingPongGame/Ball.h
+++ PingPongGame/Ball.h
@@ -10,6 +10,8 @@ class CBall {
             _speed = SPEED_BALL;
             _ball.setSize(SIZE_BALL);
             _ball.setFillColor(Color::Cyan);
+            _ball.setPosition(POS_BALL);
+            _dir = LEFT;
         }
 
         RectangleShape getBall() {
@@ -44,48 +46,93 @@ class CBall {
             }
         }
 
-        void logic(CPaddle &player1, CPaddle &player2) {
-            if (_ball.getGlobalBounds().intersects(player1.getPaddle().getGlobalBounds())) {
+        void logic(CPaddle &paddle1, CPaddle &paddle2) {
+            // bong cham paddle1
+            if (isCollision(paddle1)) {
+                _speed += 0.5f;
+                switch (_dir) {
+                    case LEFT: {
+                        if (paddle1.getPlayerServe()) {
+                            _dir = RIGHT_UP;
+                        } else {
+                            _dir = RIGHT_DOWN;
+                        }
+                    } break;
+
+                    case LEFT_DOWN: {
+                        _dir = RIGHT_DOWN;
+                    } break;
+
+                    case LEFT_UP: {
+                        _dir = RIGHT_UP;
+                    } break;
+                }
+            }
+
+            // bong cham paddle2
+            if (isCollision(paddle2)) {
+                _speed += 0.5f;
+                switch (_dir) {
+                    case RIGHT: {
+                        if (paddle2.getPlayerServe()) {
+                            _dir = LEFT_UP;
+                        } else {
+                            _dir = LEFT_DOWN;
+                        }
+                    } break;
+
+                    case RIGHT_DOWN: {
+                        _dir = LEFT_DOWN;
+                    } break;
+
+                    case RIGHT_UP: {
+                        _dir = LEFT_UP;
+                    } break;
+                }
+            }
+
+            // bong cham bien tren
+            if (_ball.getPosition().y <= 0) {
                 if (_dir == LEFT_UP) {
+                    _dir = LEFT_DOWN;
+                } else if (_dir == RIGHT_UP) {
+                    _dir = RIGHT_DOWN;
+                }
+            }
+
+            // bong cham bien duoi
+            if (_ball.getPosition().y + SIZE_BALL.y >= HEIGHT_SCREEN) {
+                if (_dir == RIGHT_DOWN) {
                     _dir = RIGHT_UP;
                 } else if (_dir == LEFT_DOWN) {
-                    _dir = RIGHT_DOWN;
-                } else if (_dir == LEFT) {
-                    if (player1.getPlayerServe()) {
-                        _dir = RIGHT_UP;
-                    } else {
-                        _dir = RIGHT_DOWN;
-                    }
-                }
-            }
-
-            if (_ball.getGlobalBounds().intersects(player2.getPaddle().getGlobalBounds())) {
-                if (_dir == RIGHT_UP) {
                     _dir = LEFT_UP;
-                } else if (_dir == RIGHT_DOWN) {
-                    _dir = LEFT_DOWN;
-                } else if (_dir == RIGHT) {
-                    if (player2.getPlayerServe()) {
-                        _dir = LEFT_UP;
-                    } else {
-                        _dir = LEFT_DOWN;
-                    }
                 }
             }
 
-            if (_ball.getPosition().x <= 0) {
-                player2.setPoint(player2.getPoint() + 1);
-                player1.setPlayerServe(true);
-                player2.setPlayerServe(false);
-            }
-
+            // p1 wins 1point
             if (_ball.getPosition().x + SIZE_BALL.x >= WIDTH_SCREEN) {
-                player1.setPoint(player1.getPoint() + 1);
-                player2.setPlayerServe(true);
-                player1.setPlayerServe(false);
+                paddle1.setPoint(paddle1.getPoint() + 1);
+                // paddle2.setPlayerServe(true);
+                // paddle1.setPlayerServe(false);
             }
 
+            // p2 wins 1point
+            if (_ball.getPosition().x <= 0) {
+                paddle2.setPoint(paddle2.getPoint() + 1);
+                // paddle2.setPlayerServe(true);
+                // paddle1.setPlayerServe(false);
+            }
+
+            // direction
             switch (_dir) {
+                case LEFT: {
+                    moveLeft();
+                } break;
+
+                case RIGHT: {
+                    moveRight();
+                } break;
+
                 case LEFT_UP: {
                     moveLeft();
                     moveUp();
@@ -94,14 +141,6 @@ class CBall {
                 case LEFT_DOWN: {
                     moveLeft();
                     moveDown();
-                } break;
-
-                case LEFT: {
-                    moveLeft();
-                } break;
-
-                case RIGHT: {
-                    moveRight();
                 } break;
 
                 case RIGHT_DOWN: {
@@ -124,6 +163,10 @@ class CBall {
         RectangleShape _ball;
         float _speed;
         int _dir;   // huong di chuyen cua bong
+
+        bool isCollision(CPaddle paddle) {
+            return _ball.getGlobalBounds().intersects(paddle.getPaddle().getGlobalBounds());
+        }
 };
 
 #endif
