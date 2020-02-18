408b99f020e08e2bcc15d69d83f24ba7d0992eee
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-02-18 15:50:15 +0700

    done logic ball

--- PingPongGame/Ball.h
+++ PingPongGame/Ball.h
@@ -56,20 +56,22 @@ class CBall {
         void logic(CPaddle &paddle1, CPaddle &paddle2) {
             // bong cham paddle1
             if (isCollision(paddle1)) {
-                _speed += 0.5f;
+                _speed += 0.25f;
+                paddle1.setSpeed(paddle1.getSpeed() + 0.25);
+                paddle2.setSpeed(paddle1.getSpeed());
                 switch (_dir) {
                     case LEFT: {
                         int a = rand() % 3;
                         switch (a) {
-                            case 0:
+                            case 2:
                                 _dir = RIGHT;
                                 break;
 
-                            case 1:
+                            case 0:
                                 _dir = RIGHT_DOWN;
                                 break;
 
-                            case 2: 
+                            case 1: 
                                 _dir = RIGHT_UP;
                                 break;
                         }
@@ -87,20 +89,22 @@ class CBall {
 
             // bong cham paddle2
             if (isCollision(paddle2)) {
-                _speed += 0.5f;
+                paddle1.setSpeed(paddle1.getSpeed() + 0.25);
+                paddle2.setSpeed(paddle1.getSpeed());
+                _speed += 0.25f;
                 switch (_dir) {
                     case RIGHT: {
                         int a = rand() % 3;
                         switch (a) {
-                            case 0: 
+                            case 1: 
                                 _dir = LEFT;
                                 break;
 
-                            case 1: 
+                            case 2: 
                                 _dir = LEFT_UP;
                                 break;
 
-                            case 2:
+                            case 0:
                                 _dir = LEFT_DOWN;
                                 break;
                         }
@@ -136,48 +140,70 @@ class CBall {
 
             // p1 wins 1point
             if (_ball.getPosition().x + SIZE_BALL.x >= WIDTH_SCREEN) {
+                paddle2.setPlayerServe(true);
                 paddle1.setPoint(paddle1.getPoint() + 1);
+
+                // set speed lai nhu cu
                 _speed = SPEED_BALL;
-                _ball.setPosition(paddle2.getPaddle().getPosition().x - SIZE_BALL.x - SIZE_PADDLE.x, paddle2.getPaddle().getPosition().y + SIZE_PADDLE.y/2);
+                paddle1.setSpeed(SPEED_PADDLE);
+                paddle2.setSpeed(SPEED_PADDLE);
+
+                cout << "p1 | p2: " << paddle1.getPoint() << " | " << paddle2.getPoint() << endl;
             }
 
             // p2 wins 1point
             if (_ball.getPosition().x <= 0) {
+                paddle1.setPlayerServe(true);
                 paddle2.setPoint(paddle2.getPoint() + 1);
+
+                // set speed lai nhu cu
                 _speed = SPEED_BALL;
+                paddle1.setSpeed(SPEED_PADDLE);
+                paddle2.setSpeed(SPEED_PADDLE);
+
+                cout << "p1 | p2: " << paddle1.getPoint() << " | " << paddle2.getPoint() << endl;
+            }
+
+            // set lai vi tri ball khi co luot choi
+            if (paddle1.getPlayerServe()) {
                 _ball.setPosition(paddle1.getPaddle().getPosition().x + SIZE_PADDLE.x, paddle1.getPaddle().getPosition().y + SIZE_PADDLE.y/2);
+            }
+            else if (paddle2.getPlayerServe()) {
+                _ball.setPosition(paddle2.getPaddle().getPosition().x - SIZE_BALL.x, paddle2.getPaddle().getPosition().y + SIZE_PADDLE.y/2);
             }
 
             // direction
-            switch (_dir) {
-                case LEFT: {
-                    moveLeft();
-                } break;
+            // chi xet _dir khi bong dang chay
+            if (!paddle1.getPlayerServe() || !paddle2.getPlayerServe())
+                switch (_dir) {
+                    case LEFT: {
+                        moveLeft();
+                    } break;
 
-                case RIGHT: {
-                    moveRight();
-                } break;
+                    case RIGHT: {
+                        moveRight();
+                    } break;
 
-                case LEFT_UP: {
-                    moveLeft();
-                    moveUp();
-                } break;
+                    case LEFT_UP: {
+                        moveLeft();
+                        moveUp();
+                    } break;
 
-                case LEFT_DOWN: {
-                    moveLeft();
-                    moveDown();
-                } break;
+                    case LEFT_DOWN: {
+                        moveLeft();
+                        moveDown();
+                    } break;
 
-                case RIGHT_DOWN: {
-                    moveRight();
-                    moveDown();
-                } break;
+                    case RIGHT_DOWN: {
+                        moveRight();
+                        moveDown();
+                    } break;
 
-                case RIGHT_UP: {
-                    moveRight();
-                    moveUp();
-                } break;
-            }
+                    case RIGHT_UP: {
+                        moveRight();
+                        moveUp();
+                    } break;
+                }
         }
 
         void drawBall(RenderWindow &window) {
