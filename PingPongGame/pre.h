8e6cc5057c5513bfdfb6513382d92b13a12efd84
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-02-19 21:52:59 +0700

    setup speed to fit with FPS

--- PingPongGame/pre.h
+++ PingPongGame/pre.h
@@ -15,7 +15,7 @@ using namespace sf;
 #define WIDTH_SCREEN 1500
 
 #define POS_BALL Vector2f(WIDTH_SCREEN / 2, HEIGHT_SCREEN / 2)
-#define SPEED_BALL 0.5f
+#define SPEED_BALL 2.5f
 #define SIZE_BALL Vector2f(25, 25)
 #define LEFT_UP 1
 #define RIGHT_UP 2
@@ -25,9 +25,9 @@ using namespace sf;
 #define LEFT 6
 
 #define SIZE_PADDLE Vector2f(25, 200)
-#define SPEED_PADDLE 1.0f
+#define SPEED_PADDLE 3.5f
 #define POS_PADDLE_1 Vector2f(0, HEIGHT_SCREEN / 2 - SIZE_PADDLE.y / 2)
-#define POS_SCORE_1 Vector2f(WIDTH_SCREEN / 2 - 60, HEIGHT_SCREEN / 2 - 75)
+#define POS_SCORE_1 Vector2f(WIDTH_SCREEN / 2 - 120, HEIGHT_SCREEN / 2 - 75)
 #define POS_PADDLE_2 Vector2f(WIDTH_SCREEN - SIZE_PADDLE.x, HEIGHT_SCREEN / 2 - SIZE_PADDLE.y / 2)
 #define POS_SCORE_2 Vector2f(WIDTH_SCREEN / 2 + 60, HEIGHT_SCREEN / 2 - 75)
 
