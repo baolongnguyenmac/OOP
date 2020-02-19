c4d8ae9d44bed0232b67e630332154fab462e130
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-02-19 21:52:17 +0700

    done menu

--- PingPongGame/Menu.h
+++ PingPongGame/Menu.h
@@ -9,25 +9,25 @@ class Menu {
     public:
         Menu(float width, float height) {
             if (!font.loadFromFile("Rainbow Colors - TTF.ttf")) {
-                // handle error
+                cout << "Loi load font\n";
             }
 
             menu[0].setFont(font);
             menu[0].setColor(sf::Color::Red);
-            menu[0].setString("Play");
-            menu[0].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 1));
+            menu[0].setString("1 player");
+            menu[0].setPosition(Vector2f(width / 2 - 120, height / (MAX_NUMBER_OF_ITEMS + 1) * 1));
             menu[0].setCharacterSize(100);
 
             menu[1].setFont(font);
             menu[1].setColor(sf::Color::White);
-            menu[1].setString("Hall cc");
-            menu[1].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
+            menu[1].setString("2 players");
+            menu[1].setPosition(Vector2f(width / 2 - 120, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
             menu[1].setCharacterSize(100);
 
             menu[2].setFont(font);
-            menu[2].setColor(sf::Color::White);
+            menu[2].setColor(Color::White);
             menu[2].setString("Exit");
-            menu[2].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
+            menu[2].setPosition(Vector2f(width / 2 - 120, height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
             menu[2].setCharacterSize(100);
 
             selectedItemIndex = 0;
@@ -39,19 +39,19 @@ class Menu {
             }
         }
 
-        void Menu::MoveUp() {
+        void MoveUp() {
             if (selectedItemIndex - 1 >= 0) {
-                menu[selectedItemIndex].setColor(sf::Color::White);
+                menu[selectedItemIndex].setColor(Color::White);
                 selectedItemIndex--;
-                menu[selectedItemIndex].setColor(sf::Color::Red);
+                menu[selectedItemIndex].setColor(Color::Red);
             }
         }
 
-        void Menu::MoveDown() {
+        void MoveDown() {
             if (selectedItemIndex + 1 < MAX_NUMBER_OF_ITEMS) {
-                menu[selectedItemIndex].setColor(sf::Color::White);
+                menu[selectedItemIndex].setColor(Color::White);
                 selectedItemIndex++;
-                menu[selectedItemIndex].setColor(sf::Color::Red);
+                menu[selectedItemIndex].setColor(Color::Red);
             }
         }
         
@@ -61,8 +61,8 @@ class Menu {
 
     private:
         int selectedItemIndex;
-        sf::Font font;
-        sf::Text menu[MAX_NUMBER_OF_ITEMS];
+        Font font;
+        Text menu[MAX_NUMBER_OF_ITEMS];
 };
 
 #endif // !MENU_H_
