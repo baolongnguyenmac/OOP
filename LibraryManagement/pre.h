80d18a895c650018a455955c2b916a6b93e910bd
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-03-26 01:01:47 +0700

    include something

---
+++ LibraryManagement/pre.h
@@ -0,0 +1,17 @@
+#ifndef PRE_H_
+#define PRE_H_
+
+#include "stdlib.h"
+#include "stdio.h"
+
+#include "iostream"
+using namespace std;
+
+#include "vector"
+#include "fstream"
+#include "string"
+
+#include "iomanip"
+#include "cmath"
+
+#endif
