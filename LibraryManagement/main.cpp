5499f2e710a1f14a56bf5230ef02422ddc5d14a2
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-03-29 00:27:59 +0700

    demo push book

--- LibraryManagement/main.cpp
+++ LibraryManagement/main.cpp
@@ -1,85 +1,12 @@
-// #include "ListBook.h"
+#include "ListBook.h"
 #include "pre.h"
 
-class CNode {
-    public:
-        CNode(int data) {
-            d = data;
-        }
-
-    private:
-        int d;
-        CNode *pNext = NULL;
-        friend class CList;
-};
-
-class CList {
-    public:
-        void push(int d) {
-            if (head == nullptr) {
-                head = tail = new CNode(d);
-            }
-            else {
-                tail->pNext = new CNode(d);
-                tail = tail->pNext;
-            }
-        }
-
-        void del(int d) {
-            CNode *del = searchNode(d);
-            if (del == NULL) {
-                return;
-            }
-
-            // node cần xoá là tail 
-            if (del->pNext == NULL) {
-                CNode *p = head;
-                for (; p->pNext != tail; p = p->pNext);
-                delete del;
-                tail = p;
-            }
-            else {
-                CNode *next = del->pNext;
-                del->d = next->d;
-                del->pNext = next->pNext;
-                delete next;
-            }
-        }
-
-        void print() {
-            for (CNode *p = head; p != nullptr; p = p->pNext) {
-                cout << p->d << " ";
-            }
-            cout << endl;
-        }
-
-        CNode* searchNode(int d) {
-            for (CNode *p = head; p != nullptr; p = p->pNext) {
-                if (p->d == d) {
-                    return p;
-                }
-            }
-            return nullptr;
-        }
-
-    private:
-        CNode *head = nullptr;
-        CNode *tail = nullptr;
-};
-
 int main(int argc, char const *argv[]) {
-    CList l;
-    l.push(1);
-    l.push(2);
-    l.push(223);
-    l.push(2212313);
-    l.push(3);
+    CListBook *listBook = CListBook::getInstance();
 
-    l.del(3);
-    l.del(1);
-    l.del(223);
+    listBook->push(CBook("meo", "meo", "meo", "meo", 12));
 
-    l.print();
+    listBook->printListBook();
 
     return 0;
 }
