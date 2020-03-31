d47516faa69e2feab54131b78e32db294ca67e7a
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-03-31 22:49:14 +0700

    write(string) file, read(string) file (binary file)

--- LibraryManagement/Book.h
+++ LibraryManagement/Book.h
@@ -6,7 +6,7 @@
 class CBook {
     public:
         friend ostream& operator<< (ostream &os, CBook book) {
-            os << "Ten sach: " << book._bookName << " || Tac gia: " << book._tacGia << " || NXB: " << book._nxb << " || ISBN: " << book._isbn;
+            os << "Ten sach: " << book._bookName << " || Tac gia: " << book._tacGia << " || NXB: " << book._nxb << " || ISBN: " << book._isbn << " || Don gia: " << book._donGia;
             return os;
         }
 
@@ -25,13 +25,13 @@ class CBook {
         int getDonGia() {
             return _donGia;
         }
-        string getPermission() {
-            return _permission;
-        }
+        // string getPermission() {
+        //     return _permission;
+        // }
 
-        void setPermission(string permission) {
-            _permission = permission;
-        }
+        // void setPermission(string permission) {
+        //     _permission = permission;
+        // }
         void setBookName(string name) {
             _bookName = name;
         }
@@ -77,15 +77,85 @@ class CBook {
             _tacGia   =tacGia;
             _donGia = donGia;
         }
-        CBook(){}
+        CBook(){
+            _bookName = "";
+            _isbn = "";
+            _nxb = "";
+            _tacGia = "";
+            _donGia = 0;
+            // _permission = "";
+        }
+
+        void readFile(istream &file) {
+            size_t size;
+            char *data;
+
+            file.read((char*) &size, sizeof(size));
+            data = new char[size+1];
+            file.read(data, size);
+            data[size]='\0';
+            _bookName = data;
+            delete data;
+
+            file.read((char*) &size, sizeof(size));
+            data = new char[size+1];
+            file.read(data, size);
+            data[size]='\0';
+            _isbn = data;
+            delete data;
+
+            file.read((char*) &size, sizeof(size));
+            data = new char[size+1];
+            file.read(data, size);
+            data[size]='\0';
+            _nxb = data;
+            delete data;
+
+            file.read((char*) &size, sizeof(size));
+            data = new char[size+1];
+            file.read(data, size);
+            data[size]='\0';
+            _tacGia = data;
+            delete data;
+
+            // file.read((char*) &size, sizeof(size));
+            // data = new char[size+1];
+            // file.read(data, size);
+            // data[size]='\0';
+            // _donGia = atoi(data);
+            // delete data;
+            file.read((char*) &_donGia, sizeof(int));
+        }
+
+        void writeFile(ostream &file) {
+            size_t size;
+
+            size = _bookName.size();
+            file.write((char*) &size, sizeof(size_t));
+            file.write( (char*)_bookName.c_str(), size);
+
+            size = _isbn.size();
+            file.write((char*) &size, sizeof(size_t));
+            file.write( (char*)_isbn.c_str(), size);
+
+            size = _nxb.size();
+            file.write((char*) &size, sizeof(size_t));
+            file.write( (char*)_nxb.c_str(), size);
+
+            size = _tacGia.size();
+            file.write((char*) &size, sizeof(size_t));
+            file.write( (char*)_tacGia.c_str(), size);
+
+            file.write((char*) &_donGia, sizeof(int));
+        }
 
     private:
-        string _bookName = "";
-        string _isbn = "";
-        string _nxb = "";
-        string _tacGia = "";
-        int _donGia = 0;
-        string _permission = "";
+        string _bookName;
+        string _isbn;
+        string _nxb;
+        string _tacGia;
+        int    _donGia;
+        // string _permission;
 };
 
 #endif
